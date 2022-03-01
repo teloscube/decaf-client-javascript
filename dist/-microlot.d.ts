d38d293c5a5533cbcaa614780312d4d14a5920c7
alioguzhan <aoguzhanyildiz@gmail.com>
2022-03-01 14:49:25 +0300

    chore: revert apollo client import statement

--- dist/-microlot.d.ts
+++ dist/-microlot.d.ts
@@ -1,7 +1,7 @@
 /**
  * This module provides internal definitions for the remote DECAF Microlot API client.
  */
-import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
+import { ApolloClient, NormalizedCacheObject } from '@apollo/client/core';
 import { Credentials } from './-auth';
 import { Remote } from './-commons';
 /**
